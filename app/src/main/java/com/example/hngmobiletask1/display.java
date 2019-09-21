cd5ef62a0d252157906b10752f8cfcafcbc215c0
Lamouresparus <loveotudor@gmail.com>
2019-09-21 07:51:31 +0100

    fixed issues with shared pref and ui

--- app/src/main/java/com/example/hngmobiletask1/display.java
+++ app/src/main/java/com/example/hngmobiletask1/display.java
@@ -14,10 +14,32 @@ public class display extends AppCompatActivity {
         setContentView(R.layout.display);
 
         SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        String display = preferences.getString("display", "");
+        String fn = preferences.getString("fn", "");
+        String ls = preferences.getString("ls", "");
 
-        TextView displayInfo = (TextView) findViewById(R.id.userName);
-        displayInfo.setText(display);
+
+        String email = preferences.getString("email", "");
+        String dob = preferences.getString("dob", "");
+        String phone = preferences.getString("phone", "");
+
+
+
+
+
+        TextView name = findViewById(R.id.userName);
+        TextView userEmail = findViewById(R.id.userEmail);
+        TextView userPhone = findViewById(R.id.userPhone);
+        TextView birthday = findViewById(R.id.userDob);
+
+
+
+
+
+        name.setText("user name : "+fn+" "+ls);
+        userEmail.setText("Email :"+email);
+        userPhone.setText("Phone Number "+phone);
+        birthday.setText("birthday : "+dob);
+
     }
 }
 
